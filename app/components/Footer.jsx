81821e2392e96c12f51771525e8f2264f4894668
joseph <langj6757@gmail.com>
2024-04-18 18:07:24 -0700

    Add footer section

---
+++ app/components/Footer.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+import { MdMail } from "react-icons/md";
+import Link from "next/link";
+const Footer = () => {
+  return (
+    <footer className="bg-blue-500 h-auto max-w-screen flex flex-col md:flex-row justify-center gap-4 md:gap-[50px] p-8">
+      <div className="max-w-[500px]">
+        <div>
+          <h2 className="text-2xl font-bold">About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            voluptas dolorum cupiditate, eos similique quae voluptate iure.
+            Ipsum est, pariatur veniam modi ad suscipit eos ut reprehenderit
+            saepe excepturi similique.
+          </p>
+          <div className="flex gap-3 mt-2">
+            <div className="hover:text-white duration-300 hover:scale-150 cursor-pointer">
+              <a href="https://www.facebook.com/" target="_blank">
+                <FaFacebook size={30} />
+              </a>
+            </div>
+            <div className="hover:text-white duration-300 hover:scale-150 cursor-pointer">
+              <a href="https://www.instagram.com/" target="_blank">
+                <FaSquareInstagram size={30} />
+              </a>
+            </div>
+            <div className="hover:text-white duration-300 hover:scale-150 cursor-pointer">
+              <a href="https://twitter.com/" target="_blank">
+                <FaSquareXTwitter size={30} />
+              </a>
+            </div>
+            <div className="hover:text-white duration-300 hover:scale-150 cursor-pointer">
+              <a href="https://youtube.com/" target="_blank">
+                <FaYoutube size={30} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold mt-2">Company</h2>
+        <div>
+          <ul>
+            <Link href="/">
+              <li className="hover:text-white duration-300 hover:ml-2">
+                About
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="hover:text-white duration-300 hover:ml-2">
+                Legal
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="hover:text-white duration-300 hover:ml-2">
+                Technology
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <div className="w-56">
+        <h2 className="text-2xl font-bold mt-2">Contact Us</h2>
+        <div>
+          <ul>
+            <li className="flex gap-1 items-center hover:text-white duration-300 hover:ml-2 cursor-pointer">
+              {" "}
+              <FaPhone /> +1 234 567 8900
+            </li>
+            <li className="w-full flex gap-1 items-center hover:text-white duration-300 hover:ml-2 cursor-pointer">
+              {" "}
+              <MdMail /> info@outervoyage.com
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./Nav/Nav";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +12,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-[5000px]">
+    <html lang="en">
       <body className={inter.className}>
         <Nav />
         <main className="pt-[80px]">{children}</main>
+        <Footer />
       </body>
     </html>
   );
